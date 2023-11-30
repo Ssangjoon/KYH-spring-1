965d7e0554e0094cf07da5356de3b53a582259e8
SangJoon Lee <qkqh34k@gmail.com>
2023-12-01 00:35:16 +0900

    step2

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,10 +1,13 @@
 package org.example.mvc;
 
-import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.ModelAndView;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,22 +20,48 @@ import java.util.List;
 public class DispatcherServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private RequestMappingHandlerMapping rmhm;
+    private List<HandlerMapping> handlerMappings;
+
+    private List<HandlerAdapter> handlerAdapters;
+
+    private List<ViewResolver> viewResolvers;
+
     @Override
     public void init() {
         RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
         rmhm.init();
+
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("org.example");
+        ahm.initialize();
+
+        handlerMappings = List.of(rmhm, ahm);
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter(), new AnnotationHandlerAdapter());
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         String requestURI = request.getRequestURI();
-        try {
-            Controller handler = rmhm.findHandler(requestURI);
-            String viewName = handler.handleRequest(request,response);
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
 
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-            requestDispatcher.forward(request, response);
+        Object handler = handlerMappings.stream()
+                .filter(hm -> hm.findHandler(new HandlerKey(requestURI, requestMethod)) != null)
+                .map(hm -> hm.findHandler(new HandlerKey(requestURI, requestMethod)))
+                .findFirst()
+                .orElseThrow(() -> new ServletException("No handler for [" + requestMethod + ", " + requestURI + "]"));
+
+        try {
+            HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                    .filter(ha -> ha.supports(handler))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No adapter for handler [" + handler + "]"));
+
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+
+            for (ViewResolver viewResolver : this.viewResolvers) {
+                View view = viewResolver.resolveViewName(modelAndView.getViewName());
+                view.render(modelAndView.getModel(), request, response);
+            }
         } catch (Throwable e) {
             logger.error("exception occurred: [{}]", e.getMessage(), e);
             throw new ServletException(e);
--- src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -7,15 +7,22 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RequestMappingHandlerMapping  {
+public class RequestMappingHandlerMapping implements HandlerMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    private Map<String, Controller> mappings = new HashMap<>();
+    private Map<HandlerKey, Controller> mappings = new HashMap<>();
 
     void init() {
-        mappings.put("/", new HomeController());
+//        mappings.put(new HandlerKey("/", RequestMethod.GET), new HomeController());
+        mappings.put(new HandlerKey("/user/form", RequestMethod.GET), new ForwardController("/user/form"));
+        mappings.put(new HandlerKey("/users", RequestMethod.GET), new UserListController());
+        mappings.put(new HandlerKey("/users", RequestMethod.POST), new UserCreateController());
+
+        mappings.keySet().forEach(path ->
+                logger.info("url path: [{}], controller: [{}]", path, mappings.get(path).getClass()));
     }
 
-    public Controller findHandler(String urlPath) {
-        return mappings.get(urlPath);
+    @Override
+    public Controller findHandler(HandlerKey handlerKey) {
+        return mappings.get(handlerKey);
     }
 }
