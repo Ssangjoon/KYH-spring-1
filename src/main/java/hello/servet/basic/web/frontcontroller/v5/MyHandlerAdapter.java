0ca09a8b72d8cc2c96dd77d34e54a5913cd9da51
SangJoon Lee <qkqh34k@gmail.com>
2024-02-08 23:48:53 +0900

    frontcontrollerv5

--- src/main/java/hello/servet/basic/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servet/basic/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -1,4 +1,15 @@
 package hello.servet.basic.web.frontcontroller.v5;
 
+import hello.servet.basic.web.frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 public interface MyHandlerAdapter {
+    boolean supports(Object handler);
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws ServletException, IOException;
 }
+
